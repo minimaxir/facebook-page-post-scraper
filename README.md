cc33d903666ad8401a7dba2d04659d4f4100b409
Max Woolf <max@minimaxir.com>
2016-04-17 22:06:44 -0700

    v2.6 support, Reactions, Unicode fixes.

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # facebook-page-post-scraper
+
+**UPDATE April 2016**: This script now supports v2.6 of the Graph API; as a result, `reactions` are used instead of `likes`, as that endpoint is more accurate.
+
 Data scraper for Facebook Pages, and also code accompanying the blog post [How to Scrape Data From Facebook Page Posts for Statistical Analysis](http://minimaxir.com/2015/07/facebook-scraper/).
 
 The actual data scraper is implemented in `get_fb_posts_fb_pages.py`; fill in the App ID and App Secret of a Facebook app you control (I strongly recommend creating an app just for this purpose) and the Page ID of the Facebook Page you want to scrape at the beginning of the file.
--- get_fb_posts_fb_page.py
+++ get_fb_posts_fb_page.py
@@ -6,7 +6,7 @@ import time
 
 app_id = <FILL IN>
 app_secret = <FILL IN> # DO NOT SHARE WITH ANYONE!
-page_id = "nytimes"
+page_id = "cnn"
 
 access_token = app_id + "|" + app_secret
 
@@ -27,12 +27,16 @@ def request_until_succeed(url):
     return response.read()
 
 
+# Needed to write tricky unicode correctly to csv; not present in tutorial
+def unicode_normalize(text):
+	return text.translate({ 0x2018:0x27, 0x2019:0x27, 0x201C:0x22, 0x201D:0x22, 0xa0:0x20 }).encode('utf-8')
+
 def getFacebookPageFeedData(page_id, access_token, num_statuses):
     
     # construct the URL string
-    base = "https://graph.facebook.com"
+    base = "https://graph.facebook.com/v2.6/"
     node = "/" + page_id + "/feed" 
-    parameters = "/?fields=message,link,created_time,type,name,id,likes.limit(1).summary(true),comments.limit(1).summary(true),shares&limit=%s&access_token=%s" % (num_statuses, access_token) # changed
+    parameters = "/?fields=message,link,created_time,type,name,id,comments.limit(1).summary(true),shares,reactions.limit(1).summary(true)&limit=%s&access_token=%s" % (num_statuses, access_token) # changed
     url = base + node + parameters
     
     # retrieve data
@@ -50,10 +54,10 @@ def processFacebookPageFeedStatus(status):
     # so must check for existence first
     
     status_id = status['id']
-    status_message = '' if 'message' not in status.keys() else status['message'].encode('utf-8')
-    link_name = '' if 'name' not in status.keys() else status['name'].encode('utf-8')
+    status_message = '' if 'message' not in status.keys() else unicode_normalize(status['message'])
+    link_name = '' if 'name' not in status.keys() else unicode_normalize(status['name'])
     status_type = status['type']
-    status_link = '' if 'link' not in status.keys() else status['link']
+    status_link = '' if 'link' not in status.keys() else unicode_normalize(status['link'])
     
     
     # Time needs special care since a) it's in UTC and
@@ -65,19 +69,19 @@ def processFacebookPageFeedStatus(status):
     
     # Nested items require chaining dictionary keys.
     
-    num_likes = 0 if 'likes' not in status.keys() else status['likes']['summary']['total_count']
+    num_reactions = 0 if 'reactions' not in status.keys() else status['reactions']['summary']['total_count']
     num_comments = 0 if 'comments' not in status.keys() else status['comments']['summary']['total_count']
     num_shares = 0 if 'shares' not in status.keys() else status['shares']['count']
     
     # return a tuple of all processed data
     return (status_id, status_message, link_name, status_type, status_link,
-           status_published, num_likes, num_comments, num_shares)
+           status_published, num_reactions, num_comments, num_shares)
 
 def scrapeFacebookPageFeedStatus(page_id, access_token):
     with open('%s_facebook_statuses.csv' % page_id, 'wb') as file:
         w = csv.writer(file)
         w.writerow(["status_id", "status_message", "link_name", "status_type", "status_link",
-           "status_published", "num_likes", "num_comments", "num_shares"])
+           "status_published", "num_reactions", "num_comments", "num_shares"])
         
         has_next_page = True
         num_processed = 0   # keep a count on how many we've processed
